c558544089446e6890f6566062487ee424cd350f
shakib-mia <smdshakibmia2001@gmail.com>
2022-04-30 11:45:25 +0600

    basic home page structure created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Header from './Shared/Header/Header';
+import Footer from './Shared/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from './Banner/Banner';
+import Inventory from './Inventory/Inventory';
+
+const Home = () => {
+      return (
+            <div>
+                  <Banner></Banner>
+                  <Inventory></Inventory>
+            </div>
+      );
+};
+
+export default Home;
