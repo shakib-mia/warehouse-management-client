ca7dbe1aebc41754d60781a2e6f9a45fe5fdb38b
shakib-mia <smdshakibmia2001@gmail.com>
2022-05-01 21:54:12 +0600

    firebase initialized

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+      apiKey: "AIzaSyA9-d0-l5RbFCr9eLPzIfh_wSF6vgtDwu4",
+      authDomain: "luxurious-cars-9cc9c.firebaseapp.com",
+      projectId: "luxurious-cars-9cc9c",
+      storageBucket: "luxurious-cars-9cc9c.appspot.com",
+      messagingSenderId: "384288017527",
+      appId: "1:384288017527:web:45c8e34de1a30a80418a72"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
