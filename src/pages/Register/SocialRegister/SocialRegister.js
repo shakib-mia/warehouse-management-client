390d24f6e052902155c1f6d62b996c1594485baa
shakib-mia <smdshakibmia2001@gmail.com>
2022-05-02 11:42:48 +0600

    github sign in implemented

--- src/pages/Register/SocialRegister/SocialRegister.js
+++ src/pages/Register/SocialRegister/SocialRegister.js
@@ -1,4 +1,4 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup, } from 'firebase/auth';
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, } from 'firebase/auth';
 import React from 'react';
 import app from '../../../firebase.init';
 
@@ -15,11 +15,21 @@ const SocialRegister = () => {
                   })
       }
 
+      const handleGithubSignIn = event => {
+            event.preventDefault();
+
+            const provider = new GithubAuthProvider();
+
+            signInWithPopup(auth, provider)
+                  .then(result => {
+                        console.log(result.user)
+                  })
+      }
+
       return (
-            <div className="row">
+            <div className="d-flex justify-content-around">
                   <button className='btn btn-outline-success my-2' onClick={HandleGoogleSignIn}>Continue with Google</button>
-                  <button className='btn btn-outline-dark my-2'>Continue with Github</button>
-                  <button className='btn btn-outline-info my-2'>Continue with Twitter</button>
+                  <button className='btn btn-outline-dark my-2' onClick={handleGithubSignIn}>Continue with Github</button>
             </div>
       );
 };
