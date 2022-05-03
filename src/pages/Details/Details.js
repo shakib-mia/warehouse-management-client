2b80b1e4977cfe3c5911ad5cd441141977416ee8
shakib-mia <smdshakibmia2001@gmail.com>
2022-05-03 18:05:28 +0600

    protected route problem solved

--- src/pages/Details/Details.js
+++ src/pages/Details/Details.js
@@ -9,28 +9,28 @@ const Details = _id => {
       const selected = data.find(car => car._id === window.location.href.substring(22));
 
       return (
-            <Container className='mt-5 pt-5'>
+            <div>
                   <Helmet>
-                        <title>
-                              {selected?.title} Details
-                        </title>
+                        <title>{selected?.title} - Luxurious Car</title>
                   </Helmet>
-                  {
-                        selected ? <Cards
-                              key={selected._id}
-                              name={selected.title}
-                              image={selected.image}
-                              description={selected.description}
-                              price={selected.price}
-                              quantity={selected.quantity}
-                              supplierName={selected.supplierName}
-                              btnLink="/"
-                              size="col-12 col-md-8 col-lg-4 mx-auto"
-                        ></Cards> : <div className="spinner-border text-center text-primary" role="status">
-                              <span className="visually-hidden">Loading...</span>
-                        </div>
-                  }
-            </Container>
+                  <Container className='mt-5 pt-5'>
+                        {
+                              selected ? <Cards
+                                    key={selected._id}
+                                    name={selected.title}
+                                    image={selected.image}
+                                    description={selected.description}
+                                    price={selected.price}
+                                    quantity={selected.quantity}
+                                    supplierName={selected.supplierName}
+                                    btnLink="/"
+                                    size="col-12 col-md-8 col-lg-4 mx-auto"
+                              ></Cards> : <div className="spinner-border text-center text-primary" role="status">
+                                    <span className="visually-hidden">Loading...</span>
+                              </div>
+                        }
+                  </Container>
+            </div>
       );
 };
 
